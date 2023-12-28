0d8f47004e137bd530cc76a758941c0be710e91e
JavierPalacios1723 <javierpalacios257@gmail.com>
2023-12-28 00:15:37 -0300

    se realizaron cambios en app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-
+const path = require("path");
 const port = 3030;
+
 app.use(express.static('public'))
+
 app.get("/",(req, res) => {
 
         res.sendFile(path.join(__dirname, "./views/home.html"))
 })
 
-app.listen(port, () =>console.log(`htpp://localhost:${port}`))
+app.listen(port, () => console.log(`htpp://localhost:${port}`));
 
  
