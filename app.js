df7cd9fa584e3958e74febc13d49b48599f42c2f
JavierPalacios1723 <javierpalacios257@gmail.com>
2023-12-28 00:21:44 -0300

    se realizaron cambios en app.js

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get("/",(req, res) => {
         res.sendFile(path.join(__dirname, "./views/home.html"))
 })
 
-app.listen(port, () => console.log(`htpp://localhost:${port}`));
+app.listen(port, () => console.log(`http://localhost:${port}`));
 
  
